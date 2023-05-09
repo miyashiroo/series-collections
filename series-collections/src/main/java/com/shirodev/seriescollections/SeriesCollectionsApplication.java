dd55e2661c082f6cadd7265f37ffc73f0dc3f45b
miyashiro <miyashiroo@gmail.com>
2023-05-09 15:34:28 -0400

    First commit with a spring project by spring initializr

---
+++ series-collections/src/main/java/com/shirodev/seriescollections/SeriesCollectionsApplication.java
@@ -0,0 +1,13 @@
+package com.shirodev.seriescollections;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeriesCollectionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeriesCollectionsApplication.class, args);
+	}
+
+}
---
+++ series-collections/src/test/java/com/shirodev/seriescollections/SeriesCollectionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shirodev.seriescollections;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeriesCollectionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
